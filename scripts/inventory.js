83d2687f2633fa42f126399a23f50b86f806ae4c
fine2006 <animeshpathak.2006@gmail.com>
2025-03-20 21:32:29 +0530

    Load all inventory screen

---
+++ scripts/inventory.js
@@ -0,0 +1,17 @@
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "http://localhost:8000/inventory/");
+xhr.send();
+xhr.responseType = "json";
+xhr.onload = () => {
+  if (xhr.readyState == 4 && xhr.status == 200) {
+    const data = xhr.response;
+    console.log(data);
+    data.forEach((item) => {
+      document.getElementById("inventoryTable").innerHTML +=
+        `<tr> <td>${item.name}</td><td>${item.price}</td><td>${item.cost}</td><td>${item.stock}</td> </tr>`;
+    });
+    console.log(document.getElementById("inventoryTable").innerHTML);
+  } else {
+    console.log(`Error: ${xhr.status}`);
+  }
+};
